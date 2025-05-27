5d568f3513d60db5534ef6b6dac5d91c50e20178
vardansinghkamra <vardan3012@gmail.com>
2025-05-27 22:09:11 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,18 +1,19 @@
 import os
 import torch
 import xml.etree.ElementTree as ET 
+import torch.utils
 import torchvision.transforms as T 
 import torchvision.transforms.functional as F 
 
 from PIL import Image
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader, random_split
 
 class VOCDataset(Dataset):
     def __init__(self, root_dir, image_set='train'):
         self.root = root_dir # Path to VOC2012
         self.image_dir = os.path.join(self.root, "JPEGImages")
         self.ann_dir = os.path.join(self.root, "Annotations")
-        image_set_file = os.path.join(self.root, "ImagesSets", "Main", f"{image_set}.txt")
+        image_set_file = os.path.join(self.root, "ImageSets", "Main", f"{image_set}.txt")
         
         with open (image_set_file, 'r') as f: 
             self.image_ids = f.readlines()
@@ -25,22 +26,22 @@ class VOCDataset(Dataset):
         
         # MAP CLASS TO A NUMBER
         self.class_dict = {name: idx for name, idx in enumerate(self.class_names)}
-        self.transform = T.Compose([T.Resize(224, 224), T.ToTensor()])
+        self.transform = T.Compose([T.Resize((224, 224)), T.ToTensor()])
         
     def __len__(self):
         return len(self.image_ids)
     
     def __getitem__(self, index):
         image_id = self.image_ids[index]
-        image_path = os.join(self.image_dir, f"{image_id}.jpg")
-        ann_path = os.join(self.ann_dir, f"{image_id}.xml")
+        image_path = os.path.join(self.image_dir, f"{image_id}.jpg")
+        ann_path = os.path.join(self.ann_dir, f"{image_id}.xml")
         
         # OPEN IMAGE
         image = Image.open(image_path).convert('RGB')
         
         # PARSE XML TREE
         tree = ET.parse(ann_path)
-        root = tree.getroot
+        root = tree.getroot()
         
         boxes = []
         labels = []
@@ -53,7 +54,7 @@ class VOCDataset(Dataset):
             
             labels.append(self.class_dict[label])
             
-            bbox = obj.find('xmin')
+            bbox = obj.find('bndbox')
             xmin = int(float(bbox.find('xmin').text))
             ymin = int(float(bbox.find('ymin').text))
             xmax = int(float(bbox.find('xmax').text))
@@ -98,4 +99,19 @@ class VOCDataset(Dataset):
 
 def collate_fn(batch):
     return tuple(zip(*batch))
+
+def get_dataloaders(batch_size=32, train_size=0.8, shuffle=True):
+    
+    assert train_size < 1 and train_size > 0, "train_size must be between 0 and 1"
+    dataset = VOCDataset(r"C:\Users\VARDAN S KAMRA\Desktop\Projects\Object Detection\VOC2012")
+    
+    train_size = int(train_size * len(dataset))
+    test_size = int(len(dataset) - train_size)
+    
+    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+    
+    train_dataloader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn, drop_last=True)
+    test_dataloader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn, drop_last=True)
+    
+    return train_dataloader, test_dataloader
 
