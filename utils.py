05baa9c1c76f0af4b21a1dd517337d4d21176868
vardansinghkamra <vardan3012@gmail.com>
2025-05-30 23:35:33 +0530

    Update utils

--- utils.py
+++ utils.py
@@ -60,21 +60,99 @@ def mAP():
     pass
 
 # Encode the Target to YOLO-Style Output
-def encode_target(image, target, S, B, C):
-    pass
+def target_to_yolo(target, S=7, B=2, C=20, image_size=(224, 224)): #(image_size is w, h)
+    # YOLO output for one grid cell looks like:
+    # [C0.......C19, Conf_B1, X_B1, Y_B1, W_B1, H_B1, Conf_B2, X_B2, Y_B2, W_B2, H_B2]
+    
+    # Only one of the two bounding box predictors (B=2) in each grid cell is filled during target preparation.
+    # This is because at train-time, only 1 predictor should be responsible for an object.
+    # We fill only the first box slot (index range C:C+5), and leave the second empty.
+    
+    # The responsibility assignment is handled during training by the loss function:
+    # During loss computation (in YOLOv1 loss), for each grid cell with an object,
+    # the box predictor whose prediction has the highest IoU with the ground truth box
+    # is assigned responsibility for that object.
+    
+    # The loss function compares both predicted boxes against the ground truth,
+    # and assigns the one with the best match to be trained for that object.
+    # The other predictor (box 2) is either ignored or penalized based on objectness loss (depending on its confidence).
+    
+    # This dynamic assignment ensures that both box predictors can learn over time,
+    # even though only one is used during labeling.
+    
+    yolo_target = torch.zeros((S, S, C+B*5))
+    boxes = target["boxes"]
+    labels = target["labels"]
+    img_w, img_h = image_size
+    
+    for i in range(len(boxes)):
+        xmin, ymin, xmax, ymax = boxes[i]
+        class_idx = labels[i]
+        
+        # Convert to centre format
+        x, y, w, h = corner_to_center(xmin, ymin, xmax, ymax)
+        
+        # Normalize between 0 and 1; remember that dataset sclaes it to 224, but here we need normalization
+        x /= img_w
+        y /= img_h
+        w /= img_w
+        h /= img_h
+        
+        # Determine the grid cell containing the centre points
+        # if x=0.4 -> grid_x = int(0.4 * 7) = 2 (starts from 0, goes till S-1)
+        grid_x = int(x * S)
+        grid_y = int(y * S)
+        
+        # If the normalized x_center or y_center is exactly 1.0 (i.e. on the far-right or bottom edge) then:
+        # grid_x = int(1.0 * S) = S
+        # This is out of bounds because valid indices are from 0 to S-1.
+        grid_x = min(grid_x, S-1)
+        grid_y = min(grid_y, S-1)
+        
+        # x and y are normalized coordinates of the object's center w.r.t. entire image
+        # If x = 0.65 and S = 7, then 0.65 * 7 = 4.55, which means the object center lies in grid cell 4 (indexing from 0), and is 0.55 units into that cell
+        # Now we need position of the object's center inside the grid cell, expressed as a value between 0 and 1
+        cell_x = x * S - grid_x
+        cell_y = y * S - grid_y
+        
+        # Skip if the grid cell is taken already (only one object per cell in YOLOv1)
+        if yolo_target[grid_y, grid_x, C] == 1:
+            continue
+        
+        yolo_target[grid_y, grid_x, class_idx] = 1                                      # one-hot class
+        yolo_target[grid_y, grid_x, C:C+5] = torch.tensor([cell_x, cell_y, w, h, 1.0])  # box + objectness
+        # In YOLOv1, each predicted bounding box has a confidence score, which is defined as:
+        # Confidence Pr(Obj) X IoU(pred, truth) 
+        # Objectness = 1 means yes, there is an object here
+        
+        # We use yolo_target[grid_y, grid_x, ...] because tensors follow [row, column] = [height, width] convention
+        # grid_y is the row (vertical axis -> height), grid_x is the column (horizontal axis -> width)
+        # This matches how image tensors are stored in PyTorch: (height, width, channels)
+        
+    return yolo_target
 
-def decode_prediction(model_output, S, B, C):
+# Encode the Linear Output to YOLO-Style Output
+def linear_to_yolo(model_output, S, B, C):
+    """ 
+    Takes a model output prediction that is (N, (S*S*(C+B*5))) and converts it to YOLO output (N, S, S, C+B*5)
+    """
+    # .view() is a reshape operation and does not detach the computation graph
+    # -1 means PyTorch infers the batch size automatically
+    return model_output.view(-1, S, S, C+B*5)
+
+# Make Boxes from YOLO-Style Output
+def yolo_to_boxes():
     pass
 
 # Corner Coordinates to Center Coordinates
-def corner_to_center(box):
-    xmin, ymin, xmax, ymax = box
-    x_center = (xmin + xmax) / 2
-    y_center = (ymin + ymax) /2
+def corner_to_center(xmin, ymin, xmax, ymax):
+    
+    x_center = (xmin + xmax) / 2.0
+    y_center = (ymin + ymax) / 2.0
     width = xmax - xmin
     height = ymax - ymin
 
-    return ([x_center, y_center, width, height])
+    return (x_center, y_center, width, height)
 
 # Visualize Bounding Boxes
 def visualize_bounding_boxes(image: torch.tensor, target, class_names):
